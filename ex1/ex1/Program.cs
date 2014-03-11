cd245118ce9377427ebc25a445c61e0eb811d7b6
Piotr Gródek <ipc0nfg@gmail.com>
2014-03-11 23:13:44 +0100

    Parsing JSON file.

---
+++ ex1/ex1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data_file = ReadArgs(args);
+
+            if (data_file == null) {
+                System.Console.WriteLine("Exiting. No data to process - invalid files. Kitten is dead.");
+                return;
+            }
+
+            ProcessData(data_file);
+        }
+
+        private static void ProcessData(StateGDPCensusData data_file)
+        {
+ 	        
+        }
+
+        private static StateGDPCensusData ReadArgs(string[] args)
+        {
+            StateGDPCensusData data = null;
+
+            if (args.Length == 0) {
+                string census_data_filepath = "CensusData.txt";  // this could be moved to settings properties
+                string state_gdp_filepath   = "StateGDP.txt";
+                System.Console.WriteLine("Using default files: [{0}] and [{1}].", census_data_filepath, state_gdp_filepath);
+                data = StateGDPCensusData.Construct(state_gdp_filepath, census_data_filepath);
+            } else if (args.Length == 2) {
+                 data = StateGDPCensusData.Construct(args[0], args[1]);
+            } else {
+                System.Console.WriteLine("Usage: <application>.exe <path>\\StateGDP.txt <path>\\CensusData.txt \n" +
+                                         "You can skip parameters to run with default, example files. Exiting.");
+                data = null;
+            }
+            
+            return data;
+        }
+    }
+}
---
+++ ex1/ex1/StateGDPCensusData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace ex1
+{
+    class StateGDPCensusData
+    {
+        public static StateGDPCensusData Construct(string file_state_gdp, string file_census)
+        {
+            StateGDPCensusData data = null;
+
+            if (File.Exists(file_state_gdp) && File.Exists(file_census))
+            {
+                data = new StateGDPCensusData();
+                
+                try
+                {
+                    using (StreamReader f_state_gdp = File.OpenText(file_state_gdp))
+                    {
+                        data.state_gdp = (JArray)JToken.ReadFrom(new JsonTextReader(f_state_gdp));
+                    }
+    
+                    using (StreamReader f_census = File.OpenText(file_census))
+                    {
+                        data.census = (JArray)JToken.ReadFrom(new JsonTextReader(f_census));
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    System.Console.WriteLine(ex);
+                    data = null;
+                }
+            }
+
+            return data;
+        }
+
+        private JArray census, state_gdp;
+
+        public JArray Census { get { return census; } }
+        public JArray StateGDP { get { return state_gdp; } }
+    }
+}
