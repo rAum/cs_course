6a544632273d31670dd422575f0aa0869055b501
Piotr Gródek <ipc0nfg@gmail.com>
2014-03-18 19:00:39 +0100

    Simple Box-Muller transform based random number generator. Output checked in Excel.

---
+++ ex2_BoxMuller/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2_BoxMuller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                BoxMuller bm = new BoxMuller();
+
+                StringBuilder sb = new StringBuilder("");
+                for (int i = 0; i < 100; ++i)
+                {
+                    sb.Append(bm.Z1);
+                    sb.Append('\t');
+                    sb.Append(bm.Z2);
+                    sb.AppendLine();
+
+                    bm.Next();
+                }
+                File.WriteAllText("boxmuller.txt", sb.ToString());
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine("Exception:", ex.ToString());
+            }
+        }
+    }
+}
